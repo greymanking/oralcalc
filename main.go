e6fd1f31612b95d4d671d0a58ec29ea155f0198d
greymanking <greyman.king@gmail.com>
2020-03-01 11:57:42 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	//"log"
+	//"net/http"
+
+	//"github.com/gin-contrib/sessions"
+	//"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	gin.SetMode(gin.ReleaseMode)
+	gin.DisableConsoleColor()
+
+	router := gin.Default()
+
+	router.Use(static.Serve("/", static.LocalFile("./public", true)))
+	router.Use(gin.Logger())
+
+	router.Run(":4000")
+}
