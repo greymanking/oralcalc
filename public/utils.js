50e682085708e22dd81e25ba88ad7f0a591e0527
greymanking <greyman.king@gmail.com>
2020-03-10 16:28:07 +0800

    format function added to Date prototype

--- public/utils.js
+++ public/utils.js
@@ -7,17 +7,18 @@ function secsToMinSecs(secs) {
   return rv;
 }
 
-function formatDate(fmt, date) {
+Date.prototype.format = function(fmt) {
   let ret;
   const opt = {
-    "Y+": date.getFullYear().toString(),        // 年
-    "m+": (date.getMonth() + 1).toString(),     // 月
-    "d+": date.getDate().toString(),            // 日
-    "H+": date.getHours().toString(),           // 时
-    "M+": date.getMinutes().toString(),         // 分
-    "S+": date.getSeconds().toString()          // 秒
+    "Y+": this.getFullYear().toString(),        // 年
+    "m+": (this.getMonth() + 1).toString(),     // 月
+    "d+": this.getDate().toString(),            // 日
+    "H+": this.getHours().toString(),           // 时
+    "M+": this.getMinutes().toString(),         // 分
+    "S+": this.getSeconds().toString()          // 秒
     // 有其他格式化字符需求可以继续添加，必须转化成字符串
   };
+
   for (let k in opt) {
     ret = new RegExp("(" + k + ")").exec(fmt);
     if (ret) {
