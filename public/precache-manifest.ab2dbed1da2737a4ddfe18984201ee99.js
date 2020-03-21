014e798274044b864b25c0608965f6187b24e273
greymanking <greyman.king@gmail.com>
2020-03-21 20:03:43 +0800

    ui shifted to react

---
+++ public/precache-manifest.ab2dbed1da2737a4ddfe18984201ee99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deda42935ecf15ac177ff3c7d5d543b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9dfec1ed5579c78a314",
+    "url": "/static/css/main.07f82f3a.chunk.css"
+  },
+  {
+    "revision": "55b9400e17cd6130663f",
+    "url": "/static/js/2.41556f7b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.41556f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9dfec1ed5579c78a314",
+    "url": "/static/js/main.207f3b7e.chunk.js"
+  },
+  {
+    "revision": "bf0b70e4f1823ece6c0b",
+    "url": "/static/js/runtime-main.8edea1ef.js"
+  }
+]);
